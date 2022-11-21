c9299e6b05215582e01d20dbfbde64237c59d7e2
Dylan du Boulay <dylanduboulay@gmail.com>
2022-11-21 10:02:57 +0800

    Contact Form functional
    Server.js removed (redundant file)
    Imported @emailjs/browser
    Can now send emails via contact form.

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,47 +1,31 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
 // import contactImg from '../assets/images/contact.png'; for a background image on the contact form.
 
 export const Contact = () => {
-    const formInitialDetails = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        message: ""
-    }
-    
-    const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const [buttonText, setButtonText] = useState("Send Message");
+    const [buttonText, setButtonText] = useState('Send');
     const [status, setStatus] = useState({});
 
-    const onFormUpdate = (catergory, value) => {
-        setFormDetails({
-            ...formDetails,
-            [catergory]: value
-        });
-    }
+    const form = useRef();
 
-    const handleFormSubmit = async (e) => {
+    const sendEmail = (e) => {
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("http://localhost:3000/home#contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8"
-            },
-            body: JSON.stringify(formDetails),
-        });
-
-        setButtonText("Send Message");
-        let result = response.json();
-        setFormDetails(formInitialDetails);
-        if(result.code === 200){
+        emailjs.sendForm('service_kryf23o', 'template_qw8biri', form.current, '3J5Jrm_qWQiM7FA-t').then((result) => {
+            console.log(result.text);
+            console.log('Email sent successfully!');
             setStatus({success: true, message: "Message sent"});
-        }
-        else{
-            setStatus({success: false, message: "An error occurred, message failed to send"});
-        };
-    };
+            e.target.reset();
+        }, (error) => {
+          console.log(error.text);
+          console.log('Email failed to send, an error occurred!');
+          setStatus({success: false, message: "An error occurred, message failed to send"});
+      }    
+      );
+      setButtonText("Send Message");
+  };
+
 
     return(
         <section className="contact" id="contact">
@@ -52,18 +36,15 @@ export const Contact = () => {
                     </Col>
                     <Col m6={6}>
                         <h2>Contact me</h2>
-                        <form onSubmit={handleFormSubmit}>
+                        <form ref={form} onSubmit={sendEmail}>
                             <Col size={12} sm={10} className="px-1">
-                                <input type="text" placeholder="First Name" name="firstName" value={formDetails.firstName} onChange={(e) => onFormUpdate('firstName', e.target.value)} required></input>
+                                <input type="text" placeholder="name" name="user_name" required></input>
                             </Col>
                             <Col size={12} sm={10} className="px-1">
-                            <input type="text" placeholder="Last Name" name="lastName" value={formDetails.lastName} onChange={(e) => onFormUpdate('lastName', e.target.value)} required></input>
+                            <input type="email" placeholder="Email" name="user_email" required></input>
                             </Col>
                             <Col size={12} sm={10} className="px-1">
-                            <input type="email" placeholder="Email" name="email" value={formDetails.email} onChange={(e) => onFormUpdate('email', e.target.value)} required></input>
-                            </Col>
-                            <Col size={12} sm={10} className="px-1">
-                            <textarea rows="6" type="text" placeholder="Message" name="message" value={formDetails.message} onChange={(e) => onFormUpdate('message', e.target.value)} required></textarea>
+                            <textarea rows="6" type="text" placeholder="Message" name="user_message" required></textarea>
                                 <button type="submit"><span>{buttonText}</span></button>
                             </Col>
                             <Col>
