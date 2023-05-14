8091bef61d1768d51575b8730dd4ca3e9ca8f27b
Dylan du Boulay <dylanduboulay@gmail.com>
2023-05-14 15:12:34 +0800

    Max width and height for project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({index, title, description, image}) => {
     return (
         <Col sm={6} md={4}>
             <div className="proj-imgbx">
-                <img src={image} alt=""></img>
+                <img src={image} alt="" style={"max-width: 660px; max height: 371.25px;" }></img>
                 <div className="proj-txt">
                     <h4>{title}</h4>
                     <span>{description}</span>
